b551eaf196cf235eb405169b134b6d924e4f11f8
zemse <sohamzemse143@gmail.com>
2020-07-18 00:14:22 +0530

    Load instances with project

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './ethereum/instances';
 
 ReactDOM.render(
   <React.StrictMode>
