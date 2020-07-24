d1408f09c9dc7a967455b081d0cbae6df44a7572
zemse <sohamzemse143@gmail.com>
2020-07-24 17:33:01 +0530

    Display NRT reward amount

--- src/components/Stakings/StakingContract/Withdraw.tsx
+++ src/components/Stakings/StakingContract/Withdraw.tsx
@@ -6,18 +6,59 @@ import '../Stakings.css';
 
 type Props = {
   instance: TimeAllyStaking;
-  startMonth: number | null;
-  endMonth: number | null;
+  startMonth: number;
+  endMonth: number;
 };
 
-export class Withdraw extends Component<Props> {
+type State = {
+  currentMonth: number | null;
+  benefits: { amount: ethers.BigNumber | null; claimed: boolean }[] | null;
+};
+
+export class Withdraw extends Component<Props, State> {
+  state: State = {
+    currentMonth: null,
+    benefits: null,
+  };
+
   instance = this.props.instance;
+  monthsArray = Object.keys([...Array(this.props.endMonth - this.props.startMonth + 1)]).map(
+    (n) => +n + this.props.startMonth
+  );
+
+  componentDidMount = async () => {
+    await this.updateNrtMonth();
+    await this.updateBenefits();
+  };
+
+  updateNrtMonth = async () => {
+    const currentMonth = await window.nrtManagerInstance.currentNrtMonth();
+
+    this.setState({ currentMonth: currentMonth.toNumber() });
+  };
+
+  updateBenefits = async () => {
+    const benefits = await Promise.all(
+      this.monthsArray.map(async (month) => {
+        if (this.state.currentMonth !== null && month > this.state.currentMonth) {
+          return { amount: null, claimed: false };
+        }
+
+        // try {
+        return {
+          amount: await this.instance.getMonthlyReward(month),
+          claimed: await this.instance.isMonthClaimed(month),
+        };
+        // } catch {
+        //   return null;
+        // }
+      })
+    );
+
+    this.setState({ benefits });
+  };
 
   render() {
-    if (this.props.startMonth === null || this.props.endMonth === null) {
-      return <>Please wait...</>;
-    }
-
     return (
       <div className="container dashboard-bg">
         <div className="row">
@@ -36,22 +77,27 @@ export class Withdraw extends Component<Props> {
                       </thead>
 
                       <tbody>
-                        {Object.keys([...Array(this.props.endMonth - this.props.startMonth + 1)])
-                          // below null coalescing is used due to a bug in typescript https://github.com/microsoft/TypeScript/issues/36436
-                          .map((n) => +n + (this.props.startMonth ?? 0))
-                          .map((month, i) => (
-                            <tr key={i}>
-                              <td>{month}</td>
-                              <td>67.78 ES </td>
-                              <td>
-                                <div className="withdraw-data-flex">
-                                  <a className="pink-btn-with">WITHDRAW</a>
-                                  <a className="btn btn-default main-btn-blue">RESTAKE </a>
-                                  <a className="btn  border-conv">CONVERT TO WES</a>
-                                </div>
-                              </td>
-                            </tr>
-                          ))}
+                        {this.monthsArray.map((month, i) => (
+                          <tr key={i}>
+                            <td>{month}</td>
+                            <td>
+                              {this.state.benefits === null
+                                ? `Loading...`
+                                : this.state.benefits[i].amount
+                                ? `${ethers.utils.formatEther(
+                                    this.state.benefits[i].amount ?? ethers.constants.Zero // Typescript bug
+                                  )} ES${this.state.benefits[i].claimed ? ` (Claimed)` : ''}`
+                                : `NRT not released`}
+                            </td>
+                            <td>
+                              <div className="withdraw-data-flex">
+                                <a className="pink-btn-with">WITHDRAW</a>
+                                <a className="btn btn-default main-btn-blue">RESTAKE </a>
+                                <a className="btn  border-conv">CONVERT TO WES</a>
+                              </div>
+                            </td>
+                          </tr>
+                        ))}
                       </tbody>
                     </Table>
                   </div>
