b2929ba13fa9249df0f869b39143d945b17f3e5b
zemse <sohamzemse143@gmail.com>
2020-07-26 17:22:09 +0530

    Fix routine for sync function

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,11 @@
 export function routine(fn: Function, msec: number): NodeJS.Timeout {
   let working = true;
-  fn().then(() => (working = false));
+  const result = fn();
+  if (result instanceof Promise) {
+    result.then(() => (working = false));
+  } else {
+    working = false;
+  }
 
   const intervalId = setInterval(async () => {
     if (!working) {
