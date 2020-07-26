35edf683df4d87e809ceb18ac7edff2105c72f67
zemse <sohamzemse143@gmail.com>
2020-07-26 18:52:51 +0530

    Update contract addresses

--- src/ethereum/instances.ts
+++ src/ethereum/instances.ts
@@ -8,14 +8,14 @@ import { PrepaidEsFactory } from './typechain/PrepaidEsFactory';
 
 const config = {
   ESN: {
-    nrtManager: '0x2BFFA44bcFa70F603357255C8BCdF4E69c71947D',
-    timeallyManager: '0xCf9d50bAc66BA0bDbddb9cEbEf6b758ff1503A05',
-    timeallyStakingTarget: '0xb12cECe1A328d737a7170959756f3E491d0aF1e8',
+    nrtManager: '0x162D0A1090435468982E890bfAc0A0A8B580d319',
+    timeallyManager: '0xfd2e3292d8eC8CD5A7eD9e232a2641C07A17C84c',
+    timeallyStakingTarget: '0xBf056B8B290e7726a426Ad168fac54F40C6Dd5F6',
     validatorSet: '0xF3b22b71F534F6aa6EEfae0dBB7b53e693b8BC07',
-    validatorManager: '0x69f288B79486Cd5D552139FeFC656F19a3Ae916F',
+    validatorManager: '0xddb6bda5a6265Bb0018b2FBb484DDc8F5DF45238',
     randomnessManager: '0xf620b0F20F90Ef5bF8C05aD65981F26775f8a32B',
     blockRewardManager: '0xe021bf70cE7C47d9744b2BdbFC7bdA1b4C7cAbD9',
-    prepaidEs: '0x8Eb81e05dbeB960909eb2e672EAd940D3B1d2649',
+    prepaidEs: '0x3b2a928bd4Ab36Dd46C4C44C4d3C2dbD60B6c092',
   },
 };
 
