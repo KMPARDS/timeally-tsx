73311a45c75d9097d06835715cfbb4a1d3f2b75d
zemse <sohamzemse143@gmail.com>
2020-07-29 23:52:39 +0530

    Change IssTime to IssTime Credit (#17)

--- src/components/Stakings/StakingContract/StakingContract.tsx
+++ src/components/Stakings/StakingContract/StakingContract.tsx
@@ -97,7 +97,7 @@ export class StakingContract extends Component<RouteComponentProps<MatchParams>,
                 </td>
               </tr>
               <tr>
-                <td>IssTime</td>
+                <td>IssTime Credit</td>
                 <td>
                   {this.state.issTime !== null
                     ? `${ethers.utils.formatEther(this.state.issTime)} ES`
