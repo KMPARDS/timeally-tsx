0c9373530892651a5960f6944921ece646d24e55
zemse <sohamzemse143@gmail.com>
2020-09-25 04:47:27 +0530

    Add kyc name to read by address

--- src/components/LoadWallet/UsingAddress.tsx
+++ src/components/LoadWallet/UsingAddress.tsx
@@ -1,63 +1,121 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Layout } from '../Layout';
 import { Card, Form, Alert, Button, Spinner } from 'react-bootstrap';
 import { ethers } from 'ethers';
+import { useHistory } from 'react-router-dom';
 
 type State = {
   addressInput: string;
   success: boolean;
 };
 
-export class UsingAddress extends Component<{}, State> {
-  state: State = {
-    addressInput: '',
-    success: false,
+export function UsingAddress() {
+  let [addressInput, setAddressInput] = useState<string>('');
+  const [displayMessage, setDisplayMessage] = useState<string>('');
+  const [isCorrectKycName, setIsCorrectKycName] = useState<boolean>(false);
+  const [isChecking, setIsChecking] = useState<boolean>(false);
+  const history = useHistory();
+
+  useEffect(() => {
+    setIsChecking(true);
+  }, [addressInput]);
+
+  console.log('hi', addressInput);
+
+  const checkedAddressInputRef = useRef('');
+
+  const savedCallback = useRef<() => Promise<void>>();
+  const callback = async () => {
+    console.log({ addressInput, checked: checkedAddressInputRef.current, isCorrectKycName });
+
+    if (
+      !ethers.utils.isHexString(addressInput) &&
+      addressInput !== checkedAddressInputRef.current
+    ) {
+      try {
+        await window.provider.resolveAddress(addressInput);
+        setIsCorrectKycName(true);
+      } catch (error) {
+        console.log('check address error', error);
+        if (isCorrectKycName) setIsCorrectKycName(false);
+      }
+      setIsChecking(false);
+      checkedAddressInputRef.current = addressInput;
+    }
   };
 
-  render() {
-    return (
-      <Layout title="Load Wallet using Address">
-        <Card>
-          <Card.Body>
-            <h4>View wallet using Address</h4>
+  useEffect(() => {
+    savedCallback.current = callback;
+  });
 
-            <p>
-              Please note that using address you will not be able to do any transaction on behalf of
-              the address. To do transaction on behalf of an address on Ethereum Blockchain, you
-              need to have the private key (it can be in form or a mnemonic, keystore or stored
-              inside your hardware wallet or metamask)
-            </p>
+  useEffect(() => {
+    console.log('hooooo', addressInput);
 
-            <Form.Group controlId="address">
-              <Form.Control
-                autoComplete="off"
-                onChange={(event) => this.setState({ addressInput: event.target.value })}
-                type="text"
-                placeholder="Enter your Address"
-                style={{ width: '325px' }}
-                isValid={ethers.utils.isAddress(this.state.addressInput)}
-                isInvalid={
-                  this.state.addressInput !== '' && !ethers.utils.isAddress(this.state.addressInput)
-                }
-              />
-            </Form.Group>
+    const intervalId = setInterval(() => {
+      if (savedCallback.current) savedCallback.current();
+    }, 1000);
 
-            <Button
-              variant="primary"
-              id="addressSubmit"
-              type="submit"
-              disabled={!ethers.utils.isAddress(this.state.addressInput)}
-              onClick={() => {
-                console.log(1234);
-                // @ts-ignore
-                window.wallet = new ethers.VoidSigner(this.state.addressInput, window.provider);
-              }}
-            >
-              Read Account
-            </Button>
-          </Card.Body>
-        </Card>
-      </Layout>
-    );
-  }
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  return (
+    <Layout title="Load Wallet using Address">
+      <Card>
+        <Card.Body>
+          <h4>View wallet using Address</h4>
+
+          <p>
+            Please note that using address you will not be able to do any transaction on behalf of
+            the address. To do transaction on behalf of an address on Ethereum Blockchain, you need
+            to have the private key (it can be in form or a mnemonic, keystore or stored inside your
+            hardware wallet or metamask)
+          </p>
+
+          <Form.Group controlId="address">
+            <Form.Control
+              autoComplete="off"
+              onChange={(event) => setAddressInput(event.target.value)}
+              type="text"
+              placeholder="Enter address or kyc name"
+              style={{ width: '325px' }}
+              isValid={
+                addressInput !== '' &&
+                (ethers.utils.isAddress(addressInput) || isCorrectKycName) &&
+                !isChecking
+              }
+              isInvalid={
+                addressInput !== '' &&
+                !ethers.utils.isAddress(addressInput) &&
+                !isCorrectKycName &&
+                !isChecking
+              }
+            />
+          </Form.Group>
+
+          {displayMessage ? <Alert variant="success">{displayMessage}</Alert> : null}
+
+          <Button
+            variant="primary"
+            id="addressSubmit"
+            type="submit"
+            disabled={(!isCorrectKycName && !ethers.utils.isAddress(addressInput)) || isChecking}
+            onClick={async () => {
+              // @ts-ignore
+              window.wallet = new ethers.VoidSigner(
+                await window.provider.resolveAddress(addressInput),
+                window.provider
+              );
+
+              setDisplayMessage('Address loaded. Redirecting to wallet page...');
+              setTimeout(() => history.push('/wallet'), 1500);
+            }}
+          >
+            Read Account
+          </Button>
+        </Card.Body>
+      </Card>
+    </Layout>
+  );
 }
