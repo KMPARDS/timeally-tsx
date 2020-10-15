11e7b3265503331b16601d503f4de2877c4cba47
zemse <sohamzemse143@gmail.com>
2020-10-15 09:30:27 +0530

    Update local chain addrs

--- src/ethereum/instances.ts
+++ src/ethereum/instances.ts
@@ -29,18 +29,18 @@ window.provider = new CustomProvider('testnet');
 
 if (process.env.REACT_APP_LOCAL_BLOCKCHAIN === 'true') {
   config.ESN = {
-    nrtManager: '0xAE519FC2Ba8e6fFE6473195c092bF1BAe986ff90',
-    timeallyManager: '0x73b647cbA2FE75Ba05B8e12ef8F8D6327D6367bF',
-    timeallyStakingTarget: '0x7d73424a8256C0b2BA245e5d5a3De8820E45F390',
-    validatorSet: '0x08425D9Df219f93d5763c3e85204cb5B4cE33aAa',
-    validatorManager: '0xA10A3B175F0f2641Cf41912b887F77D8ef34FAe8',
-    randomnessManager: '0x6E05f58eEddA592f34DD9105b1827f252c509De0',
-    blockRewardManager: '0x79EaFd0B5eC8D3f945E6BB2817ed90b046c0d0Af',
-    prepaidEs: '0x2Ce636d6240f8955d085a896e12429f8B3c7db26',
-    dayswappers: '',
-    kycdapp: '',
-    timeallyclub: '',
-    timeAllyPromotionalBucket: '',
+    nrtManager: '0x3bEb087e33eC0B830325991A32E3F8bb16A51317',
+    timeallyManager: '0xc4cfb05119Ea1F59fb5a8F949288801491D00110',
+    timeallyStakingTarget: '0x961D3860d840D6ACCeAA302fbF5C0bb83b801bb4',
+    validatorSet: '0x7eCb2899D9D66858D558391D448e6e4D1B4a86cF',
+    validatorManager: '0xcA4d0578c5e07F0964C7E7ccc87E606A234625b8',
+    randomnessManager: '0x89309551Fb7AbaaB85867ACa60404CDA649751d4',
+    blockRewardManager: '0x7F87f9830baB8A591E6f94fd1A47EE87560B0bB0',
+    prepaidEs: '0xA3C6cf908EeeebF61da6e0e885687Cab557b5e3F',
+    dayswappers: '0x8418249278d74D46014683A8029Fd6fbC88482a1',
+    kycdapp: '0xE14D14bd8D0E2c36f5E4D00106417d8cf1000e22',
+    timeallyclub: '0x44F70d80642998F6ABc424ceAf1E706a479De8Ce',
+    timeAllyPromotionalBucket: '0x2AA786Cd8544c50136e5097D5E19F6AE10E02543',
   };
 
   window.provider = new CustomProviderBase(
